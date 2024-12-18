33eebf89edc99ae8b9b926cd90493cef7dc67862
Eduarda <contatoeduardagaleno@outlook.com>
2024-12-18 13:36:20 -0300

    :rocket: Construção das Classes e Interfafce Repository :rocket:

---
+++ src/model/Caderno.ts
@@ -0,0 +1,29 @@
+import { Produto } from "./Produto";
+
+export class Caneta extends Produto{
+
+// Atributo
+   private _cor: string;
+
+// Método Construtor
+	constructor(id: number, nome: string, tipo: number, preco: number, cor: string) {
+		super(id, nome, tipo, preco);
+        this._cor = cor;
+	}
+
+// Métodos Get e Set
+
+	public get cor(): string {
+		return this._cor;
+	}
+
+	public set cor(value: string) {
+		this._cor = value;
+	}
+
+    public visualizar(): void {
+        super.visualizar();
+        console.log("cor: " + this._cor);
+    }
+    
+}
